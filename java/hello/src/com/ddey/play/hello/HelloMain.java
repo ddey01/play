387a99c91c93182c2394e4b6bb942d0b076ea783
Debashis Dey <debashis_dey@hotmail.com>
2013-06-29 20:53:12 -0700

    Changes after first push

--- java/hello/src/com/ddey/play/hello/HelloMain.java
+++ java/hello/src/com/ddey/play/hello/HelloMain.java
@@ -2,6 +2,6 @@ package com.ddey.play.hello;
 
 public class HelloMain {
 	public static void main(String[] args) {
-		System.out.println("Hello Github again");
+		System.out.println("Hello Github after push");
 	}
 }
